42bcd8dc6e99ea6f94211c73beb30d6365232de3
tngsfrontend <tngsfrontend@gmail.com>
2021-11-16 20:46:22 -0300

    feat(header): add dark-875

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -211,7 +211,7 @@ function AppBar(): JSX.Element {
                       </div>
                     )} */}
 
-                    <div className="w-auto flex items-center rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto">
+                    <div className="w-auto flex items-center rounded bg-dark-875 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto">
                       {account && chainId && userEthBalance && (
                         <>
                           <div className="px-3 py-2 text-primary text-bold">
