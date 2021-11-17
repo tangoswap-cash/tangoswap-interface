48be072f235cf8316b9810add3b8b9b58a587c9c
BitcoinIsCash <tangoswapcash@gmail.cash>
2021-11-16 22:37:15 -0300

    Revert "Swap page styles (#2)"
    This reverts commit 5f29e793cb52941ccc4f60217c1031e6edd4d35e.

--- src/pages/exchange/swap/[[...tokens]].tsx
+++ src/pages/exchange/swap/[[...tokens]].tsx
@@ -566,7 +566,7 @@ export default function Swap() {
                 {i18n._(t`Unsupported Asset`)}
               </Button>
             ) : !account ? (
-              <Web3Connect size="lg" className="w-full" style={{ backgroundColor: '#651906', color: '#E2B3B3' }} />
+              <Web3Connect size="lg" color="blue" className="w-full" />
             ) : showWrap ? (
               <Button color="gradient" size="lg" disabled={Boolean(wrapInputError)} onClick={onWrap}>
                 {wrapInputError ??
