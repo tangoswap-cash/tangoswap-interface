324f25b760192238b18e0a498de34f677a34b9bc
BitcoinIsCash <tangoswapcash@gmail.com>
2022-02-21 13:00:55 -0300

    feat: minor fixes

--- src/components/BurnedTangoCounter/index.tsx
+++ src/components/BurnedTangoCounter/index.tsx
@@ -2,24 +2,26 @@ import { useEffect, useState } from 'react';
 import { getBalanceOf } from '../../functions/getBalanceOf';
 import { useBlockNumber } from '../../state/application/hooks'
 import Image from 'next/image';
+import { useSushiContract } from '../../hooks'
 
 const burningAddress = '0x0000000000000000000000626c61636b686f6c65';
-const tangoAddress  = '0x73BE9c8Edf5e951c9a0762EA2b1DE8c8F38B5e91';
 
 const BurnedTangoCounter = () => {
   const blockNumber = useBlockNumber()
   const [burnedQty, setBurnedQty] = useState<number>(null)
+  const sushi = useSushiContract();
 
   useEffect(() => {
-    getBalanceOf(burningAddress, tangoAddress).then(balance => setBurnedQty(balance))
-  }, [blockNumber])
-  
+    getBalanceOf(sushi, burningAddress).then(balance => setBurnedQty(balance))
+  }, [blockNumber, sushi])
+
   return (
     <div className='flex items-center justify-center'>
-        <Image src="/images/animations/burn.gif" width={130} height={120} alt="Tango" />
+        <Image src="/images/animations/burn.gif" width={260} height={240} alt="Tango" />
+
         <div className='text-center'>
           <h3 className='font-bold text-2xl'>Burned</h3>
-          <p className='font-bold text-2xl'>{ burnedQty && `${burnedQty?.toLocaleString("en-US")} TANGO`}</p>
+          <p className='font-bold text-2xl'>{ burnedQty && `${burnedQty?.toLocaleString("en-US")} TANGO burned`}</p>
         </div>
     </div>
   )
--- src/functions/getBalanceOf.ts
+++ src/functions/getBalanceOf.ts
@@ -1,35 +1,9 @@
-import Web3 from 'Web3';
+import { useSushiContract } from '../hooks'
 
-export async function getBalanceOf(walletAddress: string, tokenAddress: string): Promise<number> {
-  const provider = 'https://smartbch.fountainhead.cash/mainnet'
-  const web3 = new Web3(new Web3.providers.HttpProvider(provider));
-
-  // The minimum ABI to get ERC20 Token balance
-  const minABI = [
-      // balanceOf
-      {
-          "constant":true,
-          "inputs":[{"name":"_owner","type":"address"}],
-          "name":"balanceOf",
-          "outputs":[{"name":"balance","type":"uint256"}],
-          "type":"function" as const
-      },
-      // decimals
-      {
-          "constant":true,
-          "inputs":[],
-          "name":"decimals",
-          "outputs":[{"name":"","type":"uint8"}],
-          "type":"function" as const
-      }
-  ];
-
-  const contract = new web3.eth.Contract(minABI,tokenAddress);
-  const balance = await contract.methods.balanceOf(walletAddress).call();
-  const decimals = await contract.methods.decimals().call();
-
-  const total = Math.trunc(balance/(10**decimals))
-  
-  return total;
+export async function getBalanceOf(sushi: Contract, walletAddress: string): Promise<number> {
+    const balanceOf = await sushi?.balanceOf(walletAddress);
+    const decimals = await sushi?.decimals();
+    const total = Math.trunc(balanceOf / (10 ** decimals))
+    return total;
 }
 
