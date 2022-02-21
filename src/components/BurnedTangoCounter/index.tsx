fae3f0dd219072be93adbf959778ae0f8fe284cb
tngsfrontend <93617062+tngsfrontend@users.noreply.github.com>
2022-02-21 14:08:18 -0300

    fix(BurnedTangoCounter): alignments (#22)

--- src/components/BurnedTangoCounter/index.tsx
+++ src/components/BurnedTangoCounter/index.tsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from 'react';
-import { getBalanceOf } from '../../functions/getBalanceOf';
+import { useEffect, useState } from 'react'
+import { getBalanceOf } from '../../functions/getBalanceOf'
 import { useBlockNumber } from '../../state/application/hooks'
-import Image from 'next/image';
 import { useSushiContract } from '../../hooks'
+import Image from 'next/image'
 
-const burningAddress = '0x0000000000000000000000626c61636b686f6c65';
+const burningAddress = '0x0000000000000000000000626c61636b686f6c65'
 
 const BurnedTangoCounter = () => {
   const blockNumber = useBlockNumber()
   const [burnedQty, setBurnedQty] = useState<number>(null)
-  const sushi = useSushiContract();
+  const sushi = useSushiContract()
 
   useEffect(() => {
-    getBalanceOf(sushi, burningAddress).then(balance => setBurnedQty(balance))
+    getBalanceOf(sushi, burningAddress).then((balance) => setBurnedQty(balance))
   }, [blockNumber, sushi])
 
   return (
-    <div className='flex items-center justify-center'>
-        <Image src="/images/animations/burn.gif" width={260} height={240} alt="Tango" />
+    <div className="flex items-center justify-center md:flex-col">
+      <div className="relative h-40 w-44 md:h-60 md:w-64">
+        <Image src="/images/animations/burn.gif" layout="fill" alt="Tango" />
+      </div>
 
-        <div className='text-center'>
-          <h3 className='font-bold text-2xl'>Burned</h3>
-          <p className='font-bold text-2xl'>{ burnedQty && `${burnedQty?.toLocaleString("en-US")} TANGO burned`}</p>
-        </div>
+      <div className="md:text-center">
+        <p className="font-bold text-2xl">{burnedQty && `${burnedQty?.toLocaleString('en-US')} TANGO burned`}</p>
+      </div>
     </div>
   )
 }
--- src/functions/getBalanceOf.ts
+++ src/functions/getBalanceOf.ts
@@ -1,9 +1,8 @@
-import { useSushiContract } from '../hooks'
+import { Contract } from '@ethersproject/contracts'
 
 export async function getBalanceOf(sushi: Contract, walletAddress: string): Promise<number> {
-    const balanceOf = await sushi?.balanceOf(walletAddress);
-    const decimals = await sushi?.decimals();
-    const total = Math.trunc(balanceOf / (10 ** decimals))
-    return total;
+  const balanceOf = await sushi?.balanceOf(walletAddress)
+  const decimals = await sushi?.decimals()
+  const total = Math.trunc(balanceOf / 10 ** decimals)
+  return total
 }
-
