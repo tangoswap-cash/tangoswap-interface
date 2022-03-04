c3d29552900c631621552613d15a64de0f7b7a68
BitcoinIsCash <tangoswapcash@gmail.com>
2022-03-04 13:52:03 -0300

    feat: honk 2x

--- src/pages/farm/index.tsx
+++ src/pages/farm/index.tsx
@@ -280,8 +280,6 @@ export default function Farm(): JSX.Element {
     }
   };
 
-  //TODO: BCH-HONK: 0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04-0xF2d4D9c65C2d1080ac9e1895F6a32045741831Cd: 0xD513165b3bbC1Ca812Db7CBB60340DDf74903A1c
-
   const hardcodedPairs2x = {
     [ChainId.SMARTBCH]: {
       "0xCFa5B1C5FaBF867842Ac3C25E729Fc3671d27c50": {
@@ -310,6 +308,15 @@ export default function Farm(): JSX.Element {
         rewarderId: "0xefEf4dC16316Ae8c7AF00489b0e5FA52be68D1B6",
         rewardToken: new Token(ChainId.SMARTBCH, '0x49F9ECF126B6dDF51C731614755508A4674bA7eD', 18, 'RMZ', 'Xolos'),
         rewardPerSecond: "58330000000000"
+      },
+      "0xD513165b3bbC1Ca812Db7CBB60340DDf74903A1c": {
+        farmId: 3,
+        allocPoint: 125000,
+        token0: new Token(ChainId.SMARTBCH, '0xF2d4D9c65C2d1080ac9e1895F6a32045741831Cd', 2, 'HONK', 'Honk'),
+        token1: WBCH[ChainId.SMARTBCH],
+        rewarderId: "0x3f43FF8eF6715Eb6E76452a9d719f54eFa5372b1",
+        rewardToken: new Token(ChainId.SMARTBCH, '0xF2d4D9c65C2d1080ac9e1895F6a32045741831Cd', 2, 'HONK', 'Honk'),
+        rewardPerSecond: "2325"
       },
     },
     [ChainId.SMARTBCH_AMBER]: {
