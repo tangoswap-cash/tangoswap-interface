84dca9599f62896a57059711a0e04bd3b31af3e9
BitcoinIsCash <tangoswapcash@gmail.com>
2022-06-08 23:19:12 +0200

    feat: wip

--- src/functions/parse.ts
+++ src/functions/parse.ts
@@ -8,15 +8,18 @@ export function parseBalance(value: string, decimals = 18) {
 
 // try to parse a user entered amount for a given token
 export function tryParseAmount<T extends Currency>(value?: string, currency?: T): CurrencyAmount<T> | undefined {
-  if (!value) {
-    value = "0"
-  }
-  if (!currency) {
+  // if (!value) {
+  //   value = "0"
+  // }
+  if (!value || !currency) {
     return undefined
   }
   try {
     const typedValueParsed = parseUnits(value, currency.decimals).toString()
-    return CurrencyAmount.fromRawAmount(currency, JSBI.BigInt(typedValueParsed))
+    if (typedValueParsed !== '0') {
+      return CurrencyAmount.fromRawAmount(currency, JSBI.BigInt(typedValueParsed))
+    }
+    // return CurrencyAmount.fromRawAmount(currency, JSBI.BigInt(typedValueParsed))
   } catch (error) {
     // should fail if the user specifies too many decimal places of precision (or maybe exceed max uint?)
     console.debug(`Failed to parse input amount: "${value}"`, error)
