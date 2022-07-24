58702bae294e373e5dfa5d92b4bae5affc4c4f6a
realbcher <reacrypt@protonmail.com>
2022-07-24 15:12:10 -0300

    feat: tangoprice

--- src/components/TangoPrice/index.tsx
+++ src/components/TangoPrice/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { FLEXUSD, TANGO } from '../../config/tokens'
+import { BCUSDT, FLEXUSD, TANGO } from '../../config/tokens'
 import { useV2TradeExactIn as useTradeExactIn } from '../../hooks/useV2Trades'
 import { tryParseAmount } from '../../functions/parse'
 import { ChainId } from '@tangoswapcash/sdk'
@@ -7,14 +7,14 @@ import { ChainId } from '@tangoswapcash/sdk'
 const TangoPrice = () => {
   const [price, setPrice] = useState(null)
   const parsedAmount = tryParseAmount('1', TANGO[ChainId.SMARTBCH])
-  const bestTradeExactIn = useTradeExactIn(parsedAmount, FLEXUSD)
+  const bestTradeExactIn = useTradeExactIn(parsedAmount, BCUSDT)
 
   useEffect(() => {
     if (bestTradeExactIn) setPrice(bestTradeExactIn?.executionPrice?.toSignificant(6))
   }, [bestTradeExactIn])
 
   return (
-    <div className="font-bold ml-2">
+    <div className="ml-2 font-bold">
       $<span className={!price && 'opacity-30'}>{price ? price : '0.000000'}</span>
     </div>
   )
