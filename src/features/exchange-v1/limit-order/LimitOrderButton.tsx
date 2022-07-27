5aff3d2061f2e1f79740b776ac64c682aa13e60f
BitcoinIsCash <tangoswapcash@gmail.cash>
2022-07-27 15:48:51 +0200

    feat: wip

--- src/features/exchange-v1/limit-order/LimitOrderButton.tsx
+++ src/features/exchange-v1/limit-order/LimitOrderButton.tsx
@@ -199,7 +199,7 @@ const LimitOrderButton: FC<LimitOrderButtonProps> = ({ currency, color, ...rest 
       // console.log(`order:         ${base64EncArr(hexToArr(order))}`)
 
       // const url = 'https://orders.cash/take?o=' + base64EncArr(hexToArr(order))
-      const url = 'http://localhost:3000/take-order??o=' + base64EncArr(hexToArr(order))
+      const url = 'http://localhost:3000/take-order?o=' + base64EncArr(hexToArr(order))
       console.log('url: ', url)
       setTakeOrderURL(url)
 
