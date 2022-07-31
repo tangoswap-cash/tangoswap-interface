7a7b6baa2d9c3114d9ca44dddc154726ce2b34a2
lifelessdev <ninanaaaa2@gmail.com>
2022-07-31 20:26:38 -0300

    feat: get 'TangoPrice' from coingecko api

--- src/components/TangoPrice/index.tsx
+++ src/components/TangoPrice/index.tsx
@@ -3,15 +3,21 @@ import { BCUSDT, FLEXUSD, TANGO } from '../../config/tokens'
 import { useV2TradeExactIn as useTradeExactIn } from '../../hooks/useV2Trades'
 import { tryParseAmount } from '../../functions/parse'
 import { ChainId } from '@tangoswapcash/sdk'
+import axios from 'axios'
 
 const TangoPrice = () => {
   const [price, setPrice] = useState(null)
-  const parsedAmount = tryParseAmount('1', TANGO[ChainId.SMARTBCH])
-  const bestTradeExactIn = useTradeExactIn(parsedAmount, BCUSDT)
+  // const parsedAmount = tryParseAmount('1', TANGO[ChainId.SMARTBCH])
+  // const bestTradeExactIn = useTradeExactIn(parsedAmount, BCUSDT)
 
-  useEffect(() => {
-    if (bestTradeExactIn) setPrice(bestTradeExactIn?.executionPrice?.toSignificant(6))
-  }, [bestTradeExactIn])
+  axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tangoswap&vs_currencies=usd')
+  .then(response => {
+    return setPrice(response.data.tangoswap.usd)
+  })
+
+  // useEffect(() => {
+  //   if (bestTradeExactIn) setPrice(bestTradeExactIn?.executionPrice?.toSignificant(6))
+  // }, [bestTradeExactIn])
 
   return (
     <div className="ml-2 font-bold">
