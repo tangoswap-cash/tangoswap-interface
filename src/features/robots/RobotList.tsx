091925723149d084f0e83ebbd9b491baa8ca1fa7
Santiago Benegas <santiagobenegas03@gmail.com>
2022-11-22 12:04:58 -0300

    feat: robot list style filtered

--- src/features/robots/RobotList.tsx
+++ src/features/robots/RobotList.tsx
@@ -9,20 +9,20 @@ import { useInfiniteScroll } from '../onsen/hooks'
 import RobotListItems from './RobotListItems'
 import { isMobile } from 'react-device-detect'
 
-const RobotList = ({ 
-  stockAddress, 
-  moneyAddress, 
-  robots, 
-  term, 
-  inputValue, 
-  RobotsMap, 
+const RobotList = ({
+  stockAddress,
+  moneyAddress,
+  robots,
+  term,
+  inputValue,
+  RobotsMap,
   showMaxButton,
-  onUserInput, 
-  onMax, 
-  currency, 
-  currencyB, 
-  selectedCurrencyBBalance, 
-  selectedCurrencyBalance 
+  onUserInput,
+  onMax,
+  currency,
+  currencyB,
+  selectedCurrencyBBalance,
+  selectedCurrencyBalance
 }) => {
   const { items, requestSort, sortConfig } = useSortableData(robots)
   const { i18n } = useLingui()
@@ -30,43 +30,43 @@ const RobotList = ({
 
   return items ? (
     <>
-      <div className={ "grid grid-cols-4 text-base font-bold text-primary" }>
+      <div className={window.location.href.endsWith("?filter=portfolio") ? "grid grid-cols-4 text-base font-bold text-primary space-x-3 sm:space-x-14" : "grid grid-cols-3 text-base font-bold text-primary space-x-5 sm:space-x-24 "}>
         <div
-          className="flex items-center col-span-1 sm:px-4 cursor-pointer"
+          className="flex  items-center col-span-1 px-4 cursor-pointer"
           onClick={() => requestSort('symbol')}
         >
-          <div className="hover:text-high-emphesis px-2 sm:px-4 text-sm sm:text-lg">{i18n._(t`Stock/Money`)}</div>
-          {sortConfig &&
-            sortConfig.key === 'symbol' &&
-            ((sortConfig.direction === 'ascending' && <ChevronUpIcon width={12} height={12} />) ||
-              (sortConfig.direction === 'descending' && <ChevronDownIcon width={12} height={12} />))}
-        </div>
-        <div
-          className="flex items-center justify-center pl-4 sm:px-4 cursor-pointer text-sm sm:text-lg hover:text-high-emphesis"
-          onClick={() => requestSort('lowPrice')}
-        >
-          {i18n._(t`Price to Buy`)}
-          {sortConfig &&
-            sortConfig.key === 'lowPrice' &&
-            ((sortConfig.direction === 'ascending' && <ChevronUpIcon width={12} height={12} />) ||
-              (sortConfig.direction === 'descending' && <ChevronDownIcon width={12} height={12} />))}
-        </div>
-        <div
-          className="flex items-center justify-center pl-4 sm:px-4 cursor-pointer text-sm sm:text-lg hover:text-high-emphesis"
-          onClick={() => requestSort('highPrice')}
-        >
-          {i18n._(t`Price to Sell`)}
-          {sortConfig &&
-            sortConfig.key === 'highPrice' &&
-            ((sortConfig.direction === 'ascending' && <ChevronUpIcon width={12} height={12} />) ||
-              (sortConfig.direction === 'descending' && <ChevronDownIcon width={12} height={12} />))}
-        </div>
-        <div
-          className="items-center justify-center pl-4 sm:px-4 cursor-pointer md:flex hover:text-high-emphesis"
-          
-        >
-          {i18n._(t`Balance`)}
-        </div>
+            <div className="flex hover:text-high-emphesis   text-sm sm:text-lg">{i18n._(t`Stock/Money`)}</div>
+            {sortConfig &&
+              sortConfig.key === 'symbol' &&
+              ((sortConfig.direction === 'ascending' && <ChevronUpIcon width={12} height={12} />) ||
+                (sortConfig.direction === 'descending' && <ChevronDownIcon width={12} height={12} />))}
+          </div>
+          <div
+            className={window.location.href.endsWith("?filter=sell") || window.location.href.endsWith("?filter=portfolio") ? "flex col-span-1 items-center pl-4 sm:px-3 cursor-pointer text-sm sm:text-lg hover:text-high-emphesis" : "hidden"}
+            onClick={() => requestSort('lowPrice')}
+          >
+            {i18n._(t`Price to Sell`)}
+            {sortConfig &&
+              sortConfig.key === 'lowPrice' &&
+              ((sortConfig.direction === 'ascending' && <ChevronUpIcon width={12} height={12} />) ||
+                (sortConfig.direction === 'descending' && <ChevronDownIcon width={12} height={12} />))}
+          </div>
+          <div
+            className={window.location.href.endsWith("?filter=buy") || window.location.href.endsWith("?filter=portfolio") ? "flex items-center col-span-1 pl-4 sm:px-2 cursor-pointer text-sm sm:text-lg hover:text-high-emphesis" : "hidden"}
+            onClick={() => requestSort('highPrice')}
+          >
+            {i18n._(t`Price to Buy`)}
+            {sortConfig &&
+              sortConfig.key === 'highPrice' &&
+              ((sortConfig.direction === 'ascending' && <ChevronUpIcon width={12} height={12} />) ||
+                (sortConfig.direction === 'descending' && <ChevronDownIcon width={12} height={12} />))}
+          </div>
+          <div
+            className="flex items-center col-span-1 pl-4 sm:px-4 cursor-pointer hover:text-high-emphesis"
+
+          >
+            {i18n._(t`Balance`)}
+          </div>
       </div>
       <InfiniteScroll
         dataLength={numDisplayed}
@@ -76,21 +76,21 @@ const RobotList = ({
       >
         <div className="space-y-1">
           {items.slice(0, numDisplayed).map((robot, index) => (
-          <RobotListItems 
-          stockAddress={stockAddress} 
-          moneyAddress={moneyAddress} 
-          key={index} 
-          robot={robot} 
-          inputValue={inputValue} 
-          RobotsMap={RobotsMap} 
-          showMaxButton={showMaxButton}  
-          onUserInput={onUserInput} 
-          onMax={onMax}
-          currency={currency}
-          currencyB={currencyB}
-          selectedCurrencyBBalance={selectedCurrencyBBalance}
-          selectedCurrencyBalance={selectedCurrencyBalance}
-          />
+            <RobotListItems
+              stockAddress={stockAddress}
+              moneyAddress={moneyAddress}
+              key={index}
+              robot={robot}
+              inputValue={inputValue}
+              RobotsMap={RobotsMap}
+              showMaxButton={showMaxButton}
+              onUserInput={onUserInput}
+              onMax={onMax}
+              currency={currency}
+              currencyB={currencyB}
+              selectedCurrencyBBalance={selectedCurrencyBBalance}
+              selectedCurrencyBalance={selectedCurrencyBalance}
+            />
           ))}
         </div>
       </InfiniteScroll>
